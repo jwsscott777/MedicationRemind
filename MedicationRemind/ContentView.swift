ebe81e6b04e388085e5d706b760f3400ec66261c
James Scott <tlmsols50@gmail.com>
2020-12-04 19:42:43 -0500

    Initial Commit

---
+++ MedicationRemind/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MedicationRemind
+//
+//  Created by JWSScott777 on 12/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MedicationRemind/MedicationRemindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicationRemindApp.swift
+//  MedicationRemind
+//
+//  Created by JWSScott777 on 12/4/20.
+//
+
+import SwiftUI
+
+@main
+struct MedicationRemindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
